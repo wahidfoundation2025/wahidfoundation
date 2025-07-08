57706594efb140655860624686ea98cff48dc2c2
Nuthan B <NuthanB@protonmail.com>
2025-07-08 10:32:51 +0530

    Made the footer fix

--- app/components/FooterNav.js
+++ app/components/FooterNav.js
@@ -22,7 +22,7 @@ export default function FooterNav() {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-50">
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-50 md:hidden">
       <ul className="flex justify-around items-center h-16">
         {navItems.map((item) => {
           const isActive = pathname === item.path;
